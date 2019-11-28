e87a8b949d927205435c1517fc837676eae49c89
Jens Andersson <jensandersson87@gmail.com>
2019-11-28 13:05:59 +0100

    init

---
+++ DotNetCoreStorageTable/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DotNetCoreStorageTable.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("<ENTER CONNECTION STRING>");
+
+            CloudTableClient client = storageAccount.CreateCloudTableClient();
+
+            CloudTable table = client.GetTableReference("employee");
+
+            table.CreateIfNotExistsAsync();
+
+            var emp = new EmployeeSessionEntity(1, "BillG", 123456.33);
+
+            TableOperation insertOp = TableOperation.Insert(emp);
+
+            table.ExecuteAsync(insertOp);
+        }
+    }
+}
